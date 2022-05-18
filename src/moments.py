89b2f4013a21d252a2c2ccad3c42f667d16471f3
kzhangaz <kzhangaz@connect.ust.hk>
2022-05-18 13:53:19 +0200

    fix moments

--- src/moments.py
+++ src/moments.py
@@ -3,11 +3,12 @@ from torch import *
 def moments(En):
 
 	m1 = mean(En,dim=1)
-	m2 = zeros(En.size(dim=0))
+	m2 = zeros(En.size(dim=0),En.size(dim=0))
 	
 	for j in range(En.size(dim=1)):
-		m2 = m2 + matmul(En[:,j],En[:,j])
+		m2 = m2 + mm(En[:,j],t(En[:,j]))
 
 	m2 = m2/En.size(dim=1)
 	
 	return m1,m2
+
